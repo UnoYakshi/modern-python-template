87ad54c58c58bdd3e1f31b224435882b5446741a
Uno Yakshi <uno.yakshi@gmail.com>
2024-12-03 19:22:55 +0000

    fix: Correct and enhance update_version.sh... (#24)
    Refs: #18

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+set -e
+
 # Read the version passed from semantic-release
 NEW_VERSION=$1
 
+echo "Updating __version__ to $NEW_VERSION..."
+
 # Update __init__.py with the new version
-sed -i "s/__version__ = \'.*\'/__version__ = '$NEW_VERSION'/" src/__init__.py
+sed -i "s/^__version__ = .*/__version__ = \"$NEW_VERSION\"/" src/__init__.py || {
+  echo "Failed to update __version__"; exit 1;
+}
+
+echo "Updated __version__ to $NEW_VERSION. File's content:"
+grep "__version__" src/__init__.py
