affc4f726244c7bc83056add4cb2eb50b8bbaccd
uno.yakshi <uno.yakshi@gmail.com>
2024-12-04 02:08:07 +0300

    fix: Add git add+commit+push to update_version.sh...
    Refs: #18

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -14,3 +14,11 @@ sed -i "s/^__version__ = .*/__version__ = \"$NEW_VERSION\"/" src/__init__.py || 
 
 echo "Updated __version__ to $NEW_VERSION. File's content:"
 grep "__version__" src/__init__.py
+
+
+# Add and commit changes...
+git config --global user.name "GitHub Actions"
+git config --global user.email "actions@github.com"
+git add src/__init__.py
+git commit -m "chore(version): update to ${NEW_VERSION}"
+git push origin HEAD:${GITHUB_REF}
