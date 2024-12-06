eb669e7623af95695d6816231db426dbfe49116e
uno.yakshi <uno.yakshi@gmail.com>
2024-12-06 11:21:35 +0300

    docs: Add module-docstring for src/__init.py__...
    Refs: #18

--- {{ cookiecutter.project_slug }}/src/__init__.py
+++ {{ cookiecutter.project_slug }}/src/__init__.py
@@ -1,1 +1,8 @@
+"""
+Version Information module...
+Provides the code version and is  automatically updated by the `python-semantic-release package`...
+
+WARNING: DO NOT EDIT IT MANUALLY!
+"""
+
 __version__ = "0.1.0"
